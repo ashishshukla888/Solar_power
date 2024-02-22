1fab266ddd36d6a7d7410dc2203d573f2ae1909e
ashishshukla888 <ashishshukla888.ak@gmail.com>
2024-02-22 19:54:36 +0530

    cookicutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will help in predicting the solar power output which will help to optimize the solar power prodution. ',
+    author='Ashish',
+    license='',
+)
